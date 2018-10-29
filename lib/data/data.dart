13ca0fb1b7e233a36641b4f736834c4a7712d474
iqans <iqan.shaikh@gmail.com>
2018-10-29 22:17:16 +0530

    todo data layer ready

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -34,7 +34,7 @@ class Data {
     String path = dir.path + "todos.db";
     var dbTodos = await openDatabase(path, version: 1, onCreate: _createDb);
     return dbTodos;
-  }     
+  }
       
   void _createDb(Database db, int version) async {
     String createTableQuery = "CREATE TABLE $tableTodo($columnId INTEGER PRIMARY KEY," + 
@@ -49,8 +49,28 @@ class Data {
   }
 
   Future<List> getTodos() async {
-    var db = await this.db;
+    Database db = await this.db;
     var result = await db.query(tableTodo, orderBy: columnPriority);
+    return result;
+  }
+  Future<int> getCount() async {
+    Database db = await this.db;
+    var result = Sqflite.firstIntValue(
+      await db.execute('SELECT COUNT(*) FROM $tableTodo')
+    );
+    return result;
+  }
+
+  Future<int> updateTodo(Todo todo) async {
+    Database db = await this.db;
+    var result = await db.update(tableTodo, todo.toMap(), where: columnId, whereArgs: [todo.id]);
+    return result;
+  }
+
+  
+  Future<int> deleteTodo(int id) async {
+    Database db = await this.db;
+    var result = await db.delete(tableTodo, where: columnId, whereArgs: [id]);
     return result;
   }
 }
